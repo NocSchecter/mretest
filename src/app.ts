b43db40bd513a24aea59148c25a697650aa6c0f3
Erick Aquino <erickaqge@gmail.com>
2022-11-17 18:45:23 -0500

    Se sube funcion enable\disable

--- src/app.ts
+++ src/app.ts
@@ -1,155 +1,120 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import { AlphaMode, Appearance, Material, User } from '@microsoft/mixed-reality-extension-sdk';
 
-/**
- * The main class of this app. All the logic goes here.
- */
-export default class HelloWorld {
-	private text: MRE.Actor = null;
-	private cube: MRE.Actor = null;
+export default class EnableDisable
+{
+	private model: MRE.Actor = null;
+	private buttonEnable: MRE.Actor;
+	private buttonDisable: MRE.Actor;
 	private assets: MRE.AssetContainer;
 
-	constructor(private context: MRE.Context) {
+	constructor(private context: MRE.Context)
+	{
 		this.context.onStarted(() => this.started());
 	}
 
-	/**
-	 * Once the context is "started", initialize the app.
-	 */
-	private async started() {
-		// set up somewhere to store loaded assets (meshes, textures, animations, gltfs, etc.)
+	private async started()
+	{
 		this.assets = new MRE.AssetContainer(this.context);
 
-		// Create a new actor with no mesh, but some text.
-		this.text = MRE.Actor.Create(this.context, {
-			actor: {
-				name: 'Text',
-				transform: {
-					app: { position: { x: 0, y: 0.5, z: 0 } }
-				},
-				text: {
-					contents: "Hola hypnogic",
-					anchor: MRE.TextAnchorLocation.MiddleCenter,
-					color: { r: 30 / 255, g: 206 / 255, b: 213 / 255 },
-					height: 0.3
-				}
-			}
+		const alphaMaterialEnabe = this.assets.createMaterial("alphaMat2",
+		{
+			color: {r: 0, g: 0.5838819, b: 1, a: 1},
+			alphaMode: MRE.AlphaMode.Blend
 		});
 
-		// Here we create an animation for our text actor. First we create animation data, which can be used on any
-		// actor. We'll reference that actor with the placeholder "text".
-		const spinAnimData = this.assets.createAnimationData(
-			// The name is a unique identifier for this data. You can use it to find the data in the asset container,
-			// but it's merely descriptive in this sample.
-			"Spin",
+		const alphaMaterialDisable = this.assets.createMaterial("alphaMat", 
+		{
+			color: {r: 0.4716981, g: 0.4716981, b: 0.4716981, a: 0.8},
+			alphaMode: MRE.AlphaMode.Blend
+		});
+
+		const modelData = await this.assets.loadGltf('donia.glb', 'box');
+
+		this.model = MRE.Actor.CreateFromPrefab(this.context, 
+		{
+			firstPrefabFrom: modelData,
+			actor :
 			{
-				// Animation data is defined by a list of animation "tracks": a particular property you want to change,
-				// and the values you want to change it to.
-				tracks: [{
-					// This animation targets the rotation of an actor named "text"
-					target: MRE.ActorPath("text").transform.local.rotation,
-					// And the rotation will be set to spin over 20 seconds
-					keyframes: this.generateSpinKeyframes(20, MRE.Vector3.Up()),
-					// And it will move smoothly from one frame to the next
-					easing: MRE.AnimationEaseCurves.Linear
-				}]
-			});
-		// Once the animation data is created, we can create a real animation from it.
-		spinAnimData.bind(
-			// We assign our text actor to the actor placeholder "text"
-			{ text: this.text },
-			// And set it to play immediately, and bounce back and forth from start to end
-			{ isPlaying: true, wrapMode: MRE.AnimationWrapMode.PingPong });
-
-		// Load a glTF model before we use it
-		const cubeData = await this.assets.loadGltf('altspace-cube.glb', "box");
-
-		// spawn a copy of the glTF model
-		this.cube = MRE.Actor.CreateFromPrefab(this.context, {
-			// using the data we loaded earlier
-			firstPrefabFrom: cubeData,
-			// Also apply the following generic actor properties.
-			actor: {
-				name: 'Altspace Cube',
-				// Parent the glTF model to the text actor, so the transform is relative to the text
-				parentId: this.text.id,
-				transform: {
-					local: {
-						position: { x: 0, y: -1, z: 0 },
-						scale: { x: 0.4, y: 0.4, z: 0.4 }
+				name: "Donia",
+				transform:
+				{
+					local:
+					{
+						scale: {x: 100, y: 100, z: 100},
+						position: {x: 1, y: -1, z: 1}
 					}
+				},
+				appearance: 
+				{
+					enabled: true,
+					materialId: alphaMaterialEnabe.id,
 				}
 			}
 		});
 
-		// Create some animations on the cube.
-		const flipAnimData = this.assets.createAnimationData(
-			// the animation name
-			"DoAFlip",
-			{ tracks: [{
-				// applies to the rotation of an unknown actor we'll refer to as "target"
-				target: MRE.ActorPath("target").transform.local.rotation,
-				// do a spin around the X axis over the course of one second
-				keyframes: this.generateSpinKeyframes(1.0, MRE.Vector3.Right()),
-				// and do it smoothly
-				easing: MRE.AnimationEaseCurves.Linear
-			}]}
-		);
-		// apply the animation to our cube
-		const flipAnim = await flipAnimData.bind({ target: this.cube });
+		this.buttonEnable = MRE.Actor.CreatePrimitive(this.assets,
+			{
+				definition: {shape: MRE.PrimitiveShape.Box},
+				actor:
+				{
+					transform:
+					{
+						local:
+						{
+							position: {x: -1, y: 0, z: 0},
+							scale: {x: 1, y: 1, z: 0.1}
+						}
+					},
+					appearance:
+					{
+						enabled: true,
+						materialId: alphaMaterialEnabe.id
+					}
+				},
 
-		// Set up cursor interaction. We add the input behavior ButtonBehavior to the cube.
-		// Button behaviors have two pairs of events: hover start/stop, and click start/stop.
-		const buttonBehavior = this.cube.setBehavior(MRE.ButtonBehavior);
-
-		// Trigger the grow/shrink animations on hover.
-		buttonBehavior.onHover('enter', () => {
-			// use the convenience function "AnimateTo" instead of creating the animation data in advance
-			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.5, y: 0.5, z: 0.5 } } } },
-				duration: 0.3,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
+				addCollider: true
 			});
-		});
-		buttonBehavior.onHover('exit', () => {
-			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
-				duration: 0.3,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
-			});
-		});
 
-		// When clicked, do a 360 sideways.
-		buttonBehavior.onClick(_ => {
-			flipAnim.play();
-		});
+			this.buttonEnable.created().then(() =>
+			this.buttonEnable.setBehavior(MRE.ButtonBehavior).onClick((User) => this.enable(User)))
+
+		this.buttonDisable = MRE.Actor.CreatePrimitive(this.assets,
+			{
+				definition: {shape: MRE.PrimitiveShape.Box},
+				actor:
+				{
+					transform:
+					{
+						local:
+						{
+							position: {x: 1, y: 0, z: 0},
+							scale: {x: 1, y: 1, z: 0.1}
+						}
+					},
+					appearance:
+					{
+						enabled: true,
+						materialId: alphaMaterialDisable.id
+					}
+				},
+
+				addCollider: true
+			});
+			
+			this.buttonDisable.created().then(() =>
+				this.buttonDisable.setBehavior(MRE.ButtonBehavior).onClick((User) => this.disable(User)))
 	}
 
-	/**
-	 * Generate keyframe data for a simple spin animation.
-	 * @param duration The length of time in seconds it takes to complete a full revolution.
-	 * @param axis The axis of rotation in local space.
-	 */
-	private generateSpinKeyframes(duration: number, axis: MRE.Vector3): Array<MRE.Keyframe<MRE.Quaternion>> {
-		return [{
-			time: 0 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 0)
-		}, {
-			time: 0.25 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, Math.PI / 2)
-		}, {
-			time: 0.5 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, Math.PI)
-		}, {
-			time: 0.75 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 3 * Math.PI / 2)
-		}, {
-			time: 1 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 2 * Math.PI)
-		}];
+	private enable(user: MRE.User)
+	{
+		this.model.appearance.enabled = true;
+		console.log("Activado");
+	}
+
+	private disable(user: MRE.User)
+	{
+		this.model.appearance.enabled = false;
+		console.log("Desactivado");
 	}
 }
